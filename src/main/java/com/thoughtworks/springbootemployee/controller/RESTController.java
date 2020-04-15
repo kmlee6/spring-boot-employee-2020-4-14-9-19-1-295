585bee7569fe1e3e8c0351d2b2b40625ff80d2bb
Kelvin Lee <kelvin.lee@oocl.com>
2020-04-15 18:05:02 +0800

    [RESTController] completed CRUD of employee

--- src/main/java/com/thoughtworks/springbootemployee/controller/RESTController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/RESTController.java
@@ -12,12 +12,25 @@ public class RESTController {
     private List<Employee> employeeList = new ArrayList<Employee>();
 
     @GetMapping()
-    public List<Employee> getEmployees(){
+    public List<Employee> getEmployees() {
         return employeeList;
     }
 
     @PostMapping()
-    public Employee crateEmployee(@RequestBody Employee newEmployee){
+    public Employee addEmployee(@RequestBody Employee newEmployee) {
         employeeList.add(newEmployee);
+        return newEmployee;
+    }
+
+    @PutMapping()
+    public Employee updateEmployeeInfo(@RequestBody Employee targetEmployee) {
+        employeeList.removeIf(employee -> employee.getId() == targetEmployee.getId());
+        employeeList.add(targetEmployee);
+        return targetEmployee;
+    }
+
+    @DeleteMapping()
+    public void removeEmployee(@RequestBody Employee targetEmployee) {
+        employeeList.removeIf(employee -> employee.getId() == targetEmployee.getId());
     }
 }
