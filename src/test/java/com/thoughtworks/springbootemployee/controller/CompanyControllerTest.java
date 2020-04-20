bea16d7a72ad6756680c88293b4284e0eda086d5
Kelvin Lee <kelvin.lee@oocl.com>
2020-04-20 17:00:27 +0800

    [CompanyController] Mock Service in Company Controller test case

--- src/test/java/com/thoughtworks/springbootemployee/controller/CompanyControllerTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/controller/CompanyControllerTest.java
@@ -3,7 +3,6 @@ package com.thoughtworks.springbootemployee.controller;
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.service.CompanyService;
-import com.thoughtworks.springbootemployee.service.EmployeeService;
 import io.restassured.http.ContentType;
 import io.restassured.mapper.TypeRef;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
@@ -17,13 +16,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.thoughtworks.springbootemployee.EmployeeFactory.createTestEmployees;
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -37,11 +38,21 @@ public class CompanyControllerTest {
     public void setUp() throws Exception {
         mockCompanyService= Mockito.mock(CompanyService.class);
         RestAssuredMockMvc.standaloneSetup(new CompanyController(mockCompanyService));
-        dummyCompanies.add(new Company(1, ))
+        dummyCompanies = new ArrayList<>();
+        Company google = new Company(1, "Google", new ArrayList<>());
+        List<Employee> dummyEmployees = new ArrayList<>();
+        dummyEmployees.add(new Employee(1, "Tom", 18, "Male", 20000, 1));
+        dummyEmployees.add(new Employee(1, "Tim", 18, "Male", 20000, 1));
+        dummyEmployees.add(new Employee(1, "Tommy", 18, "Male", 20000, 1));
+        google.setEmployees(dummyEmployees);
+        dummyCompanies.add(google);
+        dummyCompanies.add(new Company(2, "Facebook", new ArrayList<>()));
+        dummyCompanies.add(new Company(3, "Amazon", new ArrayList<>()));
     }
 
     @Test
     public void should_get_all_companies() {
+        doReturn(dummyCompanies).when(mockCompanyService).getAllCompanies();
         MockMvcResponse response = given().contentType(ContentType.JSON)
                 .when()
                 .get("/companies");
@@ -52,11 +63,12 @@ public class CompanyControllerTest {
                 return super.getType();
             }
         });
-        Assert.assertEquals(2, company.size());
+        Assert.assertEquals(3, company.size());
     }
 
     @Test
     public void should_get_1_company_when_page_equal_1_and_page_size_equal_to_1() {
+        doReturn(dummyCompanies.subList(1, 2)).when(mockCompanyService).getCompaniesByPage(any(), any());
         MockMvcResponse response = given().contentType(ContentType.JSON)
                 .when()
                 .get("/companies?page=2&pageSize=1");
@@ -73,6 +85,7 @@ public class CompanyControllerTest {
 
     @Test
     public void should_get_google() {
+        doReturn(dummyCompanies.get(0)).when(mockCompanyService).getCompanyById(any());
         MockMvcResponse response = given().contentType(ContentType.JSON)
                 .when()
                 .get("/companies/2");
@@ -83,6 +96,7 @@ public class CompanyControllerTest {
 
     @Test
     public void should_get_google_employees() {
+        doReturn(dummyCompanies.get(0).getEmployees()).when(mockCompanyService).getEmployeesByCompanyId(any());
         MockMvcResponse response = given().contentType(ContentType.JSON)
                 .when()
                 .get("/companies/2/employees");
